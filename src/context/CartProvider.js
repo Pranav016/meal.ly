22e542a497dabb34e50f6db31f9a6220917560f8
Pranav <pranavmendi@gmail.com>
2021-05-11 23:55:24 +0530

    Add logic for increasing cart item amount if same item is added to the Cart
    Add logic for increasing cart item amount if same item is added to the Cart instead of rendering it as another item

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -15,9 +15,29 @@ const cartActions = {
 const cartReducer = (state, action) => {
 	switch (action.type) {
 		case cartActions.ADD:
-			const updatedItems = state.items.concat(action.item);
 			const updatedTotalAmount =
 				state.totalAmount + action.item.price * action.item.amount;
+			/* if the newly added item is already present in the
+			cart then we find its index in the cart */
+			const existingCartItemIndex = state.items.findIndex(
+				(item) => item.id === action.item.id
+			);
+			const existingCartItem = state.items[existingCartItemIndex];
+			let updatedItems;
+			// case for an existing item amount is increased or decreased
+			if (existingCartItem) {
+				const updatedItem = {
+					...existingCartItem,
+					amount: existingCartItem.amount + action.item.amount,
+				};
+				updatedItems = [...state.items];
+				updatedItems[existingCartItemIndex] = updatedItem;
+			}
+			// case for a new item being added to the cart
+			else {
+				updatedItems = state.items.concat(action.item);
+			}
+
 			return {
 				items: updatedItems,
 				totalAmount: updatedTotalAmount,
