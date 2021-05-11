f5f2a6c17622822b903919acd467a717867958ac
Pranav <pranavmendi@gmail.com>
2021-05-12 01:48:17 +0530

    Add logic for removing cart item handler

--- src/context/CartProvider.js
+++ src/context/CartProvider.js
@@ -13,38 +13,54 @@ const cartActions = {
 };
 
 const cartReducer = (state, action) => {
-	switch (action.type) {
-		case cartActions.ADD:
-			const updatedTotalAmount =
-				state.totalAmount + action.item.price * action.item.amount;
-			/* if the newly added item is already present in the
+	if (action.type === cartActions.ADD) {
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+		/* if the newly added item is already present in the
 			cart then we find its index in the cart */
-			const existingCartItemIndex = state.items.findIndex(
-				(item) => item.id === action.item.id
-			);
-			const existingCartItem = state.items[existingCartItemIndex];
-			let updatedItems;
-			// case for an existing item amount is increased or decreased
-			if (existingCartItem) {
-				const updatedItem = {
-					...existingCartItem,
-					amount: existingCartItem.amount + action.item.amount,
-				};
-				updatedItems = [...state.items];
-				updatedItems[existingCartItemIndex] = updatedItem;
-			}
-			// case for a new item being added to the cart
-			else {
-				updatedItems = state.items.concat(action.item);
-			}
-
-			return {
-				items: updatedItems,
-				totalAmount: updatedTotalAmount,
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+		const existingCartItem = state.items[existingCartItemIndex];
+		let updatedItems;
+		// case for an existing item amount is increased or decreased
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
 			};
-		default:
-			return defaultCartState;
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		// case for a new item being added to the cart
+		else {
+			updatedItems = state.items.concat(action.item);
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
 	}
+	if (action.type === cartActions.REMOVE) {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+		const existingCartItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+		let updatedItems;
+		if (existingCartItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount - 1,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		return { items: updatedItems, totalAmount: updatedTotalAmount };
+	}
+	return defaultCartState;
 };
 
 const CartProvider = (props) => {
