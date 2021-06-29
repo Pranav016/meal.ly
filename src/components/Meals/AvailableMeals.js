3114dfa254a8c8e85d133eaee5749954b6a113fc
Pranav <pranavmendi@gmail.com>
2021-06-29 08:15:09 +0530

    Add functionality to fetch meals from Firebase DB and handle any errors in fetching

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,62 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99,
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5,
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99,
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99,
-	},
-];
+require('dotenv').config();
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const [meals, setMeals] = useState();
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		// a top level callback in useEffect cannot be async
+		const fetchMeals = async () => {
+			const response = await fetch(process.env.REACT_APP_FIREBASE_KEY);
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
+			const responseData = await response.json();
+
+			const laodedMeals = [];
+			for (let key in responseData) {
+				laodedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+
+			setMeals(laodedMeals);
+			setIsLoading(false);
+		};
+
+		/* fetchMeals is an async function hence returns a promise
+		 if we throw an error inside a promise, it gets rejected
+		 thus we do not use try-catch unless we do this `await fetchMeals()`
+		 which we cannot do because our callback in useEffect can't be async*/
+		fetchMeals().catch((err) => {
+			setIsLoading(false);
+			setError(err.message);
+		});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className={classes.mealsLoading}>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
+	if (error) {
+		<section className={classes.mealsError}>
+			<p>{error}</p>
+		</section>;
+	}
+
+	const mealsList = meals.map((meal) => (
 		<MealItem
 			key={meal.id}
 			id={meal.id}
